0b2745cf4e2cbaf70cd4d5bcc77ff1c60ff49cd0
frogbobber <78946249+frogbobber@users.noreply.github.com>
2022-03-24 22:19:55 -0700

    Add files via upload

--- tests/good.java
+++ tests/good.java
@@ -4,7 +4,7 @@ public class Hello {
   public static /*beepboop*/ int HelloWorld(int x) { // brynn lets figure out how to make it check the return values
     /*String should be able to be string when we add inbuilt in types*/int sus;
     sus = "yo";
-    /*String*/int b = "b";
+    // /*String*/int b = "b";
     // b++;
     // b++;
     //wrong++; // this should throw an error; this is an error for method??? brynn lets check this
@@ -22,11 +22,10 @@ public class Hello {
     Hello x = new Hello();
     int array1 = new int[10];
     int redefined;
-    int redefined;
     int redefined2 = 2;
-    int redefined2 = 3;
-    int redefined3;
-    int redefined3 = 2;
+    int redef;
+    int notredef;
+    notredef = 2;
     // errorfunc(); // this should cause an error
     // ErrorObjConstructed1 eo; // should cause an error
     // ErrorObjConstructed eo = new ErrorObjConstructed(); // this is to give an error on first part, not cosntreuctor.
@@ -36,7 +35,7 @@ public class Hello {
   public Hello() {
     int c1;
     int c2 = 6;
-    c2 = c3; //should throw an error
+    // c2 = c3; //should throw an error
   }
   // public ErrorObjConstructed() { // this will give an error becuase theres no class -- sort of counter intuitive with the tests but I belive if you have the other uncommented and this, it will get hung up on the top one, declaring the missing class and not even bother with the constructor. its just a big mess lol
   //   int c2 = 6;
